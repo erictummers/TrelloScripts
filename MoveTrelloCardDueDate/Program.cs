0e43dfa45c4664891b92dd3bcc58c35875724ec7
Eric Tummers <eric.tummers@valid.nl>
2014-12-11 16:04:15 +0100

    Using correct Architecture structure (Board>List>Card) and set ChangeSubmissionTime to force quicker sending of changed Due Date values.

--- MoveTrelloCardDueDate/Program.cs
+++ MoveTrelloCardDueDate/Program.cs
@@ -20,26 +20,27 @@ namespace MoveTrelloCardDueDate
             TrelloConfiguration.Deserializer = serializer;
             TrelloConfiguration.JsonFactory = new ManateeFactory();
             TrelloConfiguration.RestClientProvider = new RestSharpClientProvider();
+            // hate to do this, but it seems to work
+            TrelloConfiguration.ChangeSubmissionTime = TimeSpan.FromSeconds(10);
             TrelloAuthorization.Default.AppKey = TrelloIds.AppKey;
             TrelloAuthorization.Default.UserToken = TrelloIds.UserToken;
 
             var boardId = "SET_ME_TO_A_VALID_BOARDID";
             var board = new Board(boardId);
             Console.WriteLine(board.Name);
-            foreach (var card in board.Cards)
+            foreach (var list in board.Lists)
             {
-                if (card.DueDate.HasValue)
+                foreach (var card in list.Cards.Where(x => x.DueDate.HasValue))
                 {
                     // move to 20:00 on the same day
                     card.DueDate = card.DueDate.Value.Date.AddHours(20);
-                    card.Refresh();
                 }
             }
-            foreach (var card in board.Cards)
-            {
-                Console.WriteLine("{0}: {1}", card.DueDate, card.Name);
-            }
+            Console.Write("Saving {0} ...", board.Name);
+            // hate to do this, but it seems to work
+            System.Threading.Thread.Sleep(10000);
             TrelloProcessor.Shutdown();
+            Console.WriteLine("Done");
             Console.ReadLine();
         }
     }
